65156bf8760472a6f142bae204d075017aabb55d
Sami <samisamman26@yahoo.com>
2023-10-12 16:35:35 -0700

    finished count islands

--- aa06-graph-count-islands/island.js
+++ aa06-graph-count-islands/island.js
@@ -30,7 +30,7 @@ function countIslands(matrix) {
   let count = 0;
   for (let i =0; i<matrix.length; i++) {
     for (let j = 0; j<matrix[i].length; j++) {
-      if (matrix[i][j] && !visited.has(i, j)) {
+      if (matrix[i][j] && !visited.has(`[${i},${j}]`)) {
         count++;
         let stack = [[i, j]];
         visited.add(`[${i},${j}]`)
